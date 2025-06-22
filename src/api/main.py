de2fd22d401db6a4becb4304277251c9655acaed
defuzia <zhyvhyn.roman@gmail.com>
2025-06-22 03:16:58 +0300

    NEED FIX auth; feat: implement part of suggested improvements

--- src/api/main.py
+++ src/api/main.py
@@ -30,29 +30,29 @@ async def lifespan(app: FastAPI):
 
 def create_app():
     app = FastAPI(
-        title="SmartMessenger",
-        description="Smart messenger API",
+        title='SmartMessenger',
+        description='Smart messenger API',
         docs_url=settings.API_DOCS_URL,
         debug=settings.DEBUG,
         lifespan=lifespan,
+        servers=[{'url': f'http://localhost:{settings.API_PORT}'}],
+    )
+
+    app.add_middleware(
+        SessionMiddleware,
+        secret_key=settings.SECRET_KEY,
+        session_cookie='session',
+        max_age=3600,
+        same_site='lax',
     )
 
     app.add_middleware(
         CORSMiddleware,
         allow_origins=settings.CORS_ORIGINS,
         allow_credentials=True,
-        allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-        allow_headers=["Authorization", "Content-Type", "Accept"],
+        allow_methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+        allow_headers=['Authorization', 'Content-Type'],
     )
-
-    app.add_middleware(
-        SessionMiddleware,
-        secret_key=settings.SECRET_KEY,
-        session_cookie="session",
-        max_age=3600,
-        same_site="lax",
-    )
-
 
     app.include_router(v1_router, prefix=settings.API_V1_PREFIX)
     app.include_router(v1_ws_router, prefix=settings.API_V1_PREFIX)
@@ -61,11 +61,11 @@ def create_app():
     return app
 
 
-if __name__ == "__main__" and not settings.DOCKER_RUN:
+if __name__ == '__main__' and not settings.DOCKER_RUN:
     import uvicorn
 
     uvicorn.run(
-        "src.api.main:create_app",
+        'src.api.main:create_app',
         host=settings.API_HOST,
         port=settings.API_PORT,
         reload=settings.DEBUG,
